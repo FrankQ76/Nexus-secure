55d22358be7b89835f1d936e5cc9ac0139ee8ea1
TattooedGeek <17057276+FrankQ76@users.noreply.github.com>
2026-01-20 20:34:57 -0500

    chore: Update Vite server port to 6969.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 6969,
     host: '0.0.0.0',
   },
   plugins: [react(), tailwindcss()],
